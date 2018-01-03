ab0fd7adf88cbb469bfd8550875532e53378df0a
Eito Katagiri <eitoball@gmail.com>
2018-01-03 16:34:41 +0900

    implement Challenges
    * Add a Listener to the TextView
    * Add a Previous Button
    * From Button to ImageButton

--- app/src/main/java/org/eitoball/geoquiz/QuizActivity.kt
+++ app/src/main/java/org/eitoball/geoquiz/QuizActivity.kt
@@ -2,16 +2,16 @@ package org.eitoball.geoquiz
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import java.time.Duration
 
 class QuizActivity : AppCompatActivity() {
     var mTrueButton: Button? = null
     var mFalseButton: Button? = null
-    var mNextButton: Button? = null
+    var mPrevButton: ImageButton? = null
+    var mNextButton: ImageButton? = null
     var mQuestionTextView: TextView? = null
     val mQuestionBank = listOf(
             Question(R.string.question_australia, true),
@@ -28,20 +28,27 @@ class QuizActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz)
 
         mQuestionTextView = findViewById(R.id.question_text_view) as TextView
+        mQuestionTextView?.setOnClickListener { updateQuestion() }
 
         mTrueButton = findViewById(R.id.true_button) as Button
         mTrueButton?.setOnClickListener { checkAnswer(true) }
         mFalseButton = findViewById(R.id.false_button) as Button
         mFalseButton?.setOnClickListener { checkAnswer(false) }
 
-        mNextButton = findViewById(R.id.next_button) as Button
+        mPrevButton = findViewById(R.id.prev_button) as ImageButton
+        mPrevButton?.setOnClickListener { updateQuestion(-1) }
+        mNextButton = findViewById(R.id.next_button) as ImageButton
         mNextButton?.setOnClickListener { updateQuestion() }
 
         updateQuestion()
     }
 
-    private fun updateQuestion() {
-        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+    private fun updateQuestion(direction: Int = 1) {
+        mCurrentIndex += direction
+        if (mCurrentIndex < 0) {
+            mCurrentIndex = 0
+        }
+        mCurrentIndex %= mQuestionBank.size
         mQuestionTextView?.setText(mQuestionBank[mCurrentIndex].textResId)
     }
 
