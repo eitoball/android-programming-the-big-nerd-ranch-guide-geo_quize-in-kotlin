8c130d8378271580c00110e25eaa538e19dd6176
Eito Katagiri <eitoball@gmail.com>
2018-01-03 17:58:26 +0900

    3. Activity Lifecycle

--- app/src/main/java/org/eitoball/geoquiz/QuizActivity.kt
+++ app/src/main/java/org/eitoball/geoquiz/QuizActivity.kt
@@ -1,7 +1,9 @@
 package org.eitoball.geoquiz
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -9,6 +11,9 @@ import android.widget.Toast
 import java.time.Duration
 
 class QuizActivity : AppCompatActivity() {
+    val TAG = "QuizActivity"
+    val KEY_INDEX = "index"
+
     var mTrueButton: Button? = null
     var mFalseButton: Button? = null
     var mNextButton: Button? = null
@@ -25,7 +30,10 @@ class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle) called")
         setContentView(R.layout.activity_quiz)
+
+        mCurrentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
 
         mQuestionTextView = findViewById(R.id.question_text_view) as TextView
 
@@ -40,9 +48,40 @@ class QuizActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+        outState?.putInt(KEY_INDEX, mCurrentIndex)
+    }
     private fun updateQuestion() {
+        Log.d(TAG, "Current question index: $mCurrentIndex")
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
-        mQuestionTextView?.setText(mQuestionBank[mCurrentIndex].textResId)
+        try {
+            val question = mQuestionBank[mCurrentIndex]
+            mQuestionTextView?.setText(question.textResId)
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            Log.e(TAG, "Index was out of bounds", e)
+        }
     }
 
     private fun checkAnswer(userPressedTrue: Boolean) {
