b5843d7b4c6515086bd49525248b3e0d48ea0e94
Eito Katagiri <eitoball@gmail.com>
2018-01-05 06:37:04 +0900

    6. Android SDK Versions and Compatibility

--- app/src/main/java/org/eitoball/geoquiz/CheatActivity.kt
+++ app/src/main/java/org/eitoball/geoquiz/CheatActivity.kt
@@ -1,9 +1,15 @@
 package org.eitoball.geoquiz
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Button
 import android.widget.TextView
 
@@ -28,6 +34,22 @@ class CheatActivity : AppCompatActivity() {
                 mAnswerTextView?.setText(R.string.false_button)
             }
             setAnswerShownResult(true)
+
+            val cx = mShowAnswerButton?.width ?: 2 / 2
+            val cy = mShowAnswerButton?.height ?: 2 / 2
+            val radius = (mShowAnswerButton?.width ?: 1).toFloat()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0F)
+                anim.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        mShowAnswerButton?.visibility = View.INVISIBLE
+                    }
+                })
+                anim.start()
+            } else {
+                mShowAnswerButton?.visibility = View.INVISIBLE
+            }
         }
     }
 
